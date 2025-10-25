f1fb70d1754f86588bf14ccafc96726165ccdf60
balaji <Balaji.pasupuleti931@gmail.com>
2025-10-25 12:29:33 +0530

    wap to print 1-5, 50-1, 1-50+2+2

---
+++ JavaScript/for1-5.js
@@ -0,0 +1,14 @@
+// //wap to print 1 - 5 using for
+// for(let i=1; i<=5; i++){
+//     console.log(i)
+// }
+
+// //wap to print 50 - 1 using for
+// for(let i=50; i>=1; i--){
+//     console.log(i)
+// }
+
+//wap to print 1 - 50  incr2
+for(let i=1; i<=50; i=i+2){
+    console.log(i)
+}
