4da0b5441ebb971339ac44a25b8c930d386f7278
balaji <Balaji.pasupuleti931@gmail.com>
2025-10-25 14:49:39 +0530

    emp data and read

---
+++ JavaScript/seven.js
@@ -0,0 +1,17 @@
+let employees=
+[{"eid":1,"ename":"Maurice","gender":"Male"},
+{"eid":2,"ename":"Barbette","gender":"Female"},
+{"eid":3,"ename":"Ursulina","gender":"Female"},
+{"eid":4,"ename":"Julienne","gender":"Female"},
+{"eid":5,"ename":"Mabelle","gender":"Female"},
+{"eid":6,"ename":"Crystie","gender":"Female"},
+{"eid":7,"ename":"Enriqueta","gender":"Female"},
+{"eid":8,"ename":"Lilly","gender":"Female"},
+{"eid":9,"ename":"Gothart","gender":"Male"},
+{"eid":10,"ename":"Hyman","gender":"Male"}]
+
+for(let emp of employees){
+    // console.log(emp)
+    // console.log(emp.ename)
+    console.log(emp.ename,emp.gender,emp.eid)
+}
