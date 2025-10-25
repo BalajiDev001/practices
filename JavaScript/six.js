1eaef5e3b7bf7f9b1a2fcfa41785fd3ae0059975
balaji <Balaji.pasupuleti931@gmail.com>
2025-10-25 10:47:07 +0530

    meow

---
+++ JavaScript/six.js
@@ -0,0 +1,15 @@
+// let num=88;
+// if(num%2==0){
+//     console.log('its even')
+// }else{
+//     console.log('its odd')
+// }
+
+// let num=88;
+// num%2==0 ? console.log('even'):console.log('odd')
+
+// let age=18
+// age>=18 && age<=70? console.log('eligible'):console.log('not eligible')
+
+let num=233;
+num>=100 && num<=999? console.log('its a three digit number'): console.log('its not a three digit number')
