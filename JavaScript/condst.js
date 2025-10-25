b0f28b3c7ba5fcef558f16b1668af0dde7313bb7
balaji <Balaji.pasupuleti931@gmail.com>
2025-10-25 11:45:29 +0530

    conditional st

---
+++ JavaScript/condst.js
@@ -0,0 +1,14 @@
+// for (let i = 0; i <= 20; i++) {
+//   console.log(i);
+// }
+
+let score=35;
+if(score>90){
+  console.log('Very Good')
+}else if(score>50){
+  console.log('good')
+}else if(score>35){
+  console.log('improve')
+}else{
+  console.log('need more practice')
+}
